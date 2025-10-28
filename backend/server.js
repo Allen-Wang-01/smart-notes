f21c0d47dce302c28e87b4c2a9178a53786202c0
Allen Wang <wboqin@gmail.com>
2025-10-28 23:32:49 +0900

    fixed a few backend bugs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './db.js'
-
+import authRoutes from './routes/auth.js'
 dotenv.config();
 
 const app = express();
